6057c4520daae65d10322674fb66f470ea6b5140
sinclr4 <rob.sinclair@nhs.net>
2015-03-04 11:01:27 +0000

    Initial

---
+++ CHQ/Models/Topics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHQ.Models
+{
+    public class Topics
+
+    {
+        public IEnumerable<Topic> AllTopics { get; set; }
+    }
+}
